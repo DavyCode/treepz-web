83a1a67cf29a65a7a571c07a2b675b0f10b4c967
iAfolayan <iafolayanibikunle@gmail.com>
2024-03-02 22:18:34 +0100

    fix about content

--- src/components/Press/index.tsx
+++ src/components/Press/index.tsx
@@ -3,6 +3,8 @@ import OurBlogComponent from './blog';
 import SubscribeComponent from './subscribe';
 import MediaComponent from './media';
 import AnnouncementComponent from './announcement';
+import { NewsData } from '@/lib/dummyData';
+import Link from 'next/link'
 
 const PressComponent = () => {
     return (
@@ -13,92 +15,37 @@ const PressComponent = () => {
               <h1 className="text-[#6F7174] text-base font-semibold mb-8">
                 Treepz in the News
               </h1>
-              <div className="w-full h-[240px] sm:h-[344px] relative overflow-hidden rounded-2xl">
+              <div className="w-full h-[240px] sm:h-[344px] relative overflow-hidden rounded-2xl mb-6">
                 <Image
-                  src="/press-hero.png"
+                  src="/press-hero-1.png"
                   fill
                   alt="press hero"
+                  sizes="100vw, 100vh"
                   priority
                   className="object-cover absolute"
                 />
               </div>
-              <h1 className="text-2xl sm:text-[32px] font-semibold sm:leading-[40px] capitalize mt-3 sm:mt-6">
-                Nigerian MaaS startup Treepz closes $2.8M seed round to fund
-                East African expansion
-              </h1>
+              <Link href="https://techeconomy.ng/treepz-2023-in-review-transports-1-2-million-passengers-adds-six-new-cities-668-vehicles-and-more/" className="text-2xl sm:text-[32px] font-semibold sm:leading-[40px] capitalize mt-3 sm:mt-6">
+                Treepz 2023 in Review – Transports 1.2 million Passengers, Adds
+                Six New Cities, 668 Vehicles and More
+              </Link>
               <p className="text-[#6F7174] text-base capitalize mt-3 sm:mt-6">
-                Jan 17, 2023
+                Jan 24, 2024
               </p>
             </div>
             <div className="w-full sm:w-[596px] flex flex-col space-y-6 pb-6 pt-6 sm:pt-14">
-              <div className="w-full flex flex-col space-y-3 border-b pb-6">
-                <div className="w-[131px] h-5 relative">
-                  <Image
-                    src="/TechCr.png"
-                    fill
-                    priority
-                    className="object-contain absolute"
-                    alt="company"
-                  />
+              {NewsData.map(({logo, title, date, href}, index) => (
+                <div className="w-full flex flex-col space-y-3 border-b pb-6" key={index}>
+                 {logo}
+                  <Link href={href} className="capitalize text-gray-900 text-base w-full my-3 font-semibold">
+                    {title}
+                  </Link>
+                  <p className="text-[#6F7174] text-base capitalize mt-6">
+                    {date}
+                  </p>
                 </div>
-                <p className="capitalize text-gray-900 text-base w-full my-3 font-semibold">
-                  Nigerian MaaS startup Treepz closes $2.8M seed round to fund
-                </p>
-                <p className="text-[#6F7174] text-base capitalize mt-6">
-                  Jan 17, 2023
-                </p>
-              </div>
-              <div className="w-full flex flex-col space-y-3 border-b pb-6">
-                <div className="w-[131px] h-5 relative">
-                  <Image
-                    src="/TechCr.png"
-                    fill
-                    priority
-                    className="object-contain absolute"
-                    alt="company"
-                  />
-                </div>
-                <p className="capitalize text-gray-900 text-base w-full my-3 font-semibold">
-                  Nigerian MaaS startup Treepz closes $2.8M seed round to fund
-                </p>
-                <p className="text-[#6F7174] text-base capitalize mt-6">
-                  Jan 17, 2023
-                </p>
-              </div>
-              <div className="w-full flex flex-col space-y-3 border-b pb-6">
-                <div className="w-[131px] h-5 relative">
-                  <Image
-                    src="/TechCr.png"
-                    fill
-                    priority
-                    className="object-contain absolute"
-                    alt="company"
-                  />
-                </div>
-                <p className="capitalize text-gray-900 text-base w-full my-3 font-semibold">
-                  Nigerian MaaS startup Treepz closes $2.8M seed round to fund
-                </p>
-                <p className="text-[#6F7174] text-base capitalize mt-6">
-                  Jan 17, 2023
-                </p>
-              </div>
-              <div className="w-full flex flex-col space-y-3 border-b pb-6">
-                <div className="w-[131px] h-5 relative">
-                  <Image
-                    src="/TechCr.png"
-                    fill
-                    priority
-                    className="object-contain absolute"
-                    alt="company"
-                  />
-                </div>
-                <p className="capitalize text-gray-900 text-base w-full my-3 font-semibold">
-                  Nigerian MaaS startup Treepz closes $2.8M seed round to fund
-                </p>
-                <p className="text-[#6F7174] text-base capitalize mt-6">
-                  Jan 17, 2023
-                </p>
-              </div>
+
+              ))}
             </div>
           </div>
           {/* Blog */}
--- src/components/common/testimony.tsx
+++ src/components/common/testimony.tsx
@@ -4,49 +4,53 @@ import ArrowRightIcon from '@/assets/svgs/arrow-right-line.svg'
 
 const Testimony = () => {
     return (
-      <div className="flex w-full justify-between h-[620px] items-center mt-14 flex-col sm:flex-row">
+      <div className="flex w-full justify-between h-[671px] items-center mt-14 flex-col sm:flex-row">
         <div className="w-full sm:w-[640px] h-[671px] relative overflow-hidden">
           <Image
             src="/testimony-hero.png"
             alt="testimony"
             fill
             priority
-            className="object-cover sm:object-contain absolute"
+            className="object-cover absolute"
           />
         </div>
-        <div className="bg-[#FDF2D0] sm:py-[106px] sm:px-[120px] w-full sm:w-[872px]">
-          <div className="flex justify-between items-center px-4 sm:px-0 py-8 sm:py-0">
-            <span className="border border-gray-900 rounded-full">
+        <div className="bg-[#FDF2D0] w-full sm:w-[872px] sm:h-[671px] flex justify-center items-center flex-col">
+          <div className="flex justify-center items-center px-4 sm:px-0 py-8 sm:py-0">
+            {/* <span className="border border-gray-900 rounded-full">
               <ArrowLeftIcon />
-            </span>
+            </span> */}
             <div className="w-full sm:w-[162px] h-14 relative">
               <Image
                 alt="elevation"
-                src="/testimony/brighter-money.png"
+                src="/testimony/Techstars.png"
                 fill
+                sizes='100vw, 100vh'
                 priority
-                className="object-contain absolute"
+                className="object-cover absolute"
               />
             </div>
-            <span className="border border-gray-900 rounded-full">
+            {/*  <span className="border border-gray-900 rounded-full">
               <ArrowRightIcon />
+            </span> */}
+          </div>
+          <div className="text-center text-xl sm:text-2xl font-medium mt-2 sm:mt-14 px-4 w-full sm:w-[627px] mx-auto text-[#6F7174]">
+            &quot;Treepz made our Techstars Lagos program unforgettable by
+            providing top-notch transportation for our tech startup founders and
+            CEOs. The buses were modern, clean, and the entire process was
+            efficient. Treepz exceeded our expectations, and we look forward to
+            future collaborations!&quot;
+          </div>
+          <div className="flex flex-col mt-8 w-full sm:w-[326px] h-12 text-center text- mb-14 mx-auto">
+            Oluwadunni Fanibe,
+            <span className="font-semibold">
+              Techstars Lagos Program Manager
             </span>
           </div>
-          <div className="text-center text-xl sm:text-2xl font-medium mt-2 sm:mt-14 px-4">
-            &quot;For Twitter, the most important thing when producing events is
-            to trust our vendors, and Treepz delivered. I didn&lsquo;t have one
-            worry knowing Treepz was on the ground to support Twitter and our
-            200+ person event.&quot;
-          </div>
-          <div className="flex flex-col mt-6 w-full sm:w-[185px] h-12 text-center text- mb-14 mx-auto">
-            Pastor Godman,{" "}
-            <span className="font-semibold">Pastor, Elevation Church</span>
-          </div>
-          <div className="flex justify-between items-center mb-4 sm:mb-0 w-[50px] mx-auto">
+          {/*  <div className="flex justify-between items-center mb-4 sm:mb-0 w-[50px] mx-auto">
             <span className="w-[12px] h-[12px] rounded-full bg-[#212529]"></span>
             <span className="w-[12px] h-[12px] rounded-full bg-[#212529]/60"></span>
             <span className="w-[12px] h-[12px] rounded-full bg-[#212529]/60"></span>
-          </div>
+          </div> */}
         </div>
       </div>
     );
