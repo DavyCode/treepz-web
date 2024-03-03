fc71094b6d2a44e0549c558bb7b9361ee541f1c8
iAfolayan <iafolayanibikunle@gmail.com>
2024-03-03 15:58:24 +0100

    fix group images

--- src/components/Wedding/index.tsx
+++ src/components/Wedding/index.tsx
@@ -6,11 +6,14 @@ import WeddingFeatures from './features';
 import Testimony from '../common/testimony';
 import WeddingPostal from './card';
 import { WeddingPostalData } from '@/lib/dummyData'
+import { useModal } from "@/contexts/ModalContext";
 
 const WeddingComponent = () => {
+       const { showModal } = useModal();
+
     return (
         <>
-            <div className="w-full bg-[#FDF3D8] pt-5 sm:pt-[38px] pb-5 sm:pb-[60px]">
+            <div className="w-full bg-[#FDF3D8] pt-5 sm:pt-[38px] pb-5 sm:pb-[60px] mt-10 sm:mt-20">
                 <div className="container px-4 sm:px-20 flex flex-col gap-5 sm:flex-row justify-between">
                     <div className="w-full sm:w-[622px] pt-[32px] sm:pt-[64px] px-5 sm:px-10 pb-[30px] sm:pb-[90px] flex flex-col bg-white space-y-4 sm:space-y-8 rounded-2xl">
                         <h1 className="text-[28px] sm:text-[56px] font-bold text-[#212529] sm:leading-[64px]">Treepz for Weddings</h1>
@@ -20,11 +23,15 @@ const WeddingComponent = () => {
                         </div>
                         <Button
                             variant={"default"}
-                            className="cursor-pointer rounded-full w-fit sm:w-[151px] mt-8 sm:mt-[60px] text-black flex items-center gap-2"
-                            //onClick={() => {}}
-                        >
-                            Book a vehicle
-                            <ChevronRightIcon />
+                            className="relative w-fit sm:w-[151px] sm:h-[64px] group overflow-hidden mt-8 sm:mt-[60px]"
+                            onClick={showModal}
+                            >
+                            <span className="absolute top-0 left-0 w-full h-full flex items-center justify-center transition-transform duration-700 ease-in-out group-hover:transform group-hover:-translate-y-full">
+                                Book a vehicle
+                            </span>
+                            <span className="absolute top-0 left-0 w-full h-full flex items-center justify-center transition-transform duration-700 ease-in-out transform translate-y-full group-hover:translate-y-0">
+                                Book a vehicle
+                            </span>
                         </Button>
                     </div>
                     <div className="w-full sm:w-[622px] h-[360px] sm:h-[620px] relative overflow-hidden rounded-2xl">
@@ -51,10 +58,15 @@ const WeddingComponent = () => {
                         <p className="text-xl text-center">Use the button below to get in touch with us</p>
                         <Button
                             variant={"default"}
-                            className="cursor-pointer rounded-full font-medium text-gray-900 flex items-center gap-2 mt-6 w-fit sm:mt-10 sm:mb-[48px] mx-auto"
-                            //onClick={() => {}}
-                        >
-                            Contact us
+                            className="relative w-fit sm:w-[151px] sm:h-[64px] group overflow-hidden sm:mt-10 sm:mb-[48px] mx-auto flex justify-center"
+                            onClick={() => router.push("/contact")}
+                            >
+                            <span className="absolute top-0 left-0 w-full h-full flex items-center justify-center transition-transform duration-700 ease-in-out group-hover:transform group-hover:-translate-y-full">
+                                Contact us
+                            </span>
+                            <span className="absolute top-0 left-0 w-full h-full flex items-center justify-center transition-transform duration-700 ease-in-out transform translate-y-full group-hover:translate-y-0">
+                                Contact us
+                            </span>
                         </Button>
                     </div>
             </div>
